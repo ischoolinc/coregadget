a458df6d0eed77ffbf27ea745036ac8954ded81d
ChunTai Chen <chuntaichen@ChunTaide-MacBook-Pro.local>
2018-10-16 15:49:42 +0800

    [康橋專案]-Behavior 新增:修改與刪除Comment功能。

--- d10a1a37-37a5-4778-a10c-194fceac5e29/src/app/app.module.ts
+++ d10a1a37-37a5-4778-a10c-194fceac5e29/src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GadgetService } from './gadget.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { MatDialogModule, MatFormFieldModule,MatInputModule } from '@angular/material';
+import { ConfirmDialogComponent } from './behavior/confirm-dialog.component';
+import { ConfirmCancelComponent } from './behavior/confirm-cancel.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ConfirmDialogComponent,
+    ConfirmCancelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [GadgetService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    ConfirmDialogComponent,
+    ConfirmCancelComponent
+  ]
 })
 export class AppModule { }
--- d10a1a37-37a5-4778-a10c-194fceac5e29/src/app/behavior/list/list.component.ts
+++ d10a1a37-37a5-4778-a10c-194fceac5e29/src/app/behavior/list/list.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import * as moment from 'moment';
-import { GadgetService, Contract } from 'src/app/gadget.service';
-import { Utils } from 'src/app/util';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import * as moment from "moment";
+import { GadgetService, Contract } from "src/app/gadget.service";
+import { Utils } from "src/app/util";
+import { DialogService } from "../dialog-service.service";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['../common.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["../common.css"]
 })
 export class ListComponent implements OnInit {
   head: string;
@@ -19,9 +20,12 @@ export class ListComponent implements OnInit {
   courseID: string;
   courseName: string;
 
-  constructor(private route: ActivatedRoute, private gadget: GadgetService) {
+  constructor(
+    private route: ActivatedRoute,
+    private gadget: GadgetService,
+    private dialogService: DialogService
+  ) {
     // 取得 contract 連線。
-
   }
   contract: Contract;
 
@@ -29,10 +33,53 @@ export class ListComponent implements OnInit {
     this.courseID = this.route.snapshot.paramMap.get("id");
     this.courseName = this.route.snapshot.paramMap.get("name");
     // console.log(this.courseID);
-    this.contract = await this.gadget.getContract('kcis');
+    this.contract = await this.gadget.getContract("kcis");
     this.getData();
+  }
 
+  // 修改 comment
+  async editComment(data) {
+    const result = await this.dialogService.confirmCancel("Edit "+data.Name, "Comment");
+    if (result.confirm) {
+      try {
+        const rsp = await this.contract.send("behavior.EditBehaviorData", {
+          Request: {
+            BehaviorData: {
+              Field: { Comment: result.reason },
+              Condition: { Uid: data.BehaviorUID }
+            }
+          }
+        });
+        this.getData();
+      } catch (error) {
+        alert(error);
+      } finally {
+      }
+    }
+    // console.log(data);
+  }
 
+  // 刪除資料
+  async delete(data) {
+    const result = await this.dialogService.confirm(
+      "Confirmation",
+      "Are you sure want to delete the comment?"
+    );
+    if (result) {
+      try {
+        // 確認刪除
+        const rsp = await this.contract.send("behavior.DelBehaviorData", {
+          Request: { BehaviorData: { Condition: { Uid: data.BehaviorUID } } }
+        });
+        this.getData();
+      } catch (error) {
+        alert(error);
+      } finally {
+      }
+    } else {
+      return;
+    }
+    // console.log(data);
   }
 
   async getData() {
@@ -40,11 +87,14 @@ export class ListComponent implements OnInit {
       this.loading = true;
 
       // 呼叫 service。
-      const rsp = await this.contract.send('behavior.GetBehaviorDataByCourseID', {
-        Request: {
-          CourseID: this.courseID
+      const rsp = await this.contract.send(
+        "behavior.GetBehaviorDataByCourseID",
+        {
+          Request: {
+            CourseID: this.courseID
+          }
         }
-      });
+      );
       this.behaviorDataInfo = Utils.array(rsp, "Response/BehaviorData");
       this.behaviorDataList = [];
       for (const data of this.behaviorDataInfo) {
@@ -52,7 +102,6 @@ export class ListComponent implements OnInit {
       }
 
       // console.log(this.behaviorDataList);
-
     } catch (err) {
       console.log(err);
     } finally {
