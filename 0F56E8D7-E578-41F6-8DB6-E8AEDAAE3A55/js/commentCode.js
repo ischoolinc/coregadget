b12c9f1c20e0e4219e793f6f9560bebab407df03
Grady_Liu <grady.liu@ischool.com.tw>
2018-12-25 10:43:54 +0800

    「成績輸入（新）」新增匯出報表功能、文字評量代碼表畫面

---
+++ 0F56E8D7-E578-41F6-8DB6-E8AEDAAE3A55/js/commentCode.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('commentCode',[]);
+
+app.controller('commentCodeCtrl', ['$scope', function ($scope) {
+
+    $scope.connection = gadget.getContract("ta");
+    // 取得平時評量代碼表
+    $scope.connection.send({
+        service: "TeacherAccess.GetExamTextScoreMappingTable",
+        body: {
+            Content: {}
+        },
+        result: function (response, error, http) {
+            if (error !== null) {
+                alert("TeacherAccess.GetExamTextScoreMappingTable Error");
+            } else {
+                $scope.$apply(function () {
+                    if (response !== null && response.Response !== null && response.Response !== '') {
+
+                        $scope.examTextList = [].concat(response.Response.TextMappingTable.Item);
+                    }
+                });
+            }
+        }
+    });
+}]);
