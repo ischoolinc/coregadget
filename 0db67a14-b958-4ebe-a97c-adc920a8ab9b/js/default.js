af043661cee39a06918c02c49bc7ea43d1c28071
Grady_Liu <grady.liu@ischool.com.tw>
2019-01-11 11:09:48 +0800

    測驗日期驗證邏輯優化

--- 0db67a14-b958-4ebe-a97c-adc920a8ab9b/js/default.js
+++ 0db67a14-b958-4ebe-a97c-adc920a8ab9b/js/default.js
@@ -18,18 +18,26 @@ function isValidDate2(s) {
     }
     else {
         var bits = s.split('/');
-        var y = bits[0],
-            m = bits[1],
-            d = bits[2];
-        // Assume not leap year by default (note zero index for Jan)
-        var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-        // If evenly divisible by 4 and not evenly divisible by 100,
-        // or is evenly divisible by 400, then a leap year
-        if ((!(y % 4) && y % 100) || !(y % 400)) {
-            daysInMonth[1] = 29;
+        var y = parseInt(bits[0]), 
+            m = parseInt(bits[1]),
+            d = parseInt(bits[2]);
+        if (isNaN(y) || isNaN(m) || isNaN(d)) {
+            return false;
         }
-        return d <= daysInMonth[--m]
+        if (y == 0 || m == 0 || d == 0){
+            return false;
+        }
+        else {
+            // Assume not leap year by default (note zero index for Jan)
+            var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+            // If evenly divisible by 4 and not evenly divisible by 100,
+            // or is evenly divisible by 400, then a leap year
+            if ((!(y % 4) && y % 100) || !(y % 400)) {
+                daysInMonth[1] = 29;
+            }
+            return d <= daysInMonth[--m]
+        }
     }
 }
 var app = angular
