f50a82e17ff8e0aec85782522453d93634c3e849
Lelala <lelala@ischool.com.tw>
2019-06-28 11:10:16 +0800

    新增列印學生報表功能、學生報表設計工具、綜合紀錄表題目設計工具

---
+++ C7FF82EC-F412-4566-ACDE-4888751F70E0/src/app/simple-page/editor/comprehensive-editor/comprehensive-editor.component.ts
@@ -0,0 +1,211 @@
+import { Component, OnInit, Optional, ViewChild, TemplateRef } from "@angular/core";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-comprehensive-editor',
+  templateUrl: './comprehensive-editor.component.html',
+  styleUrls: ['./comprehensive-editor.component.css']
+})
+export class ComprehensiveEditorComponent implements OnInit {
+
+  questionSubject: any[] = [];
+  requireList: any[];
+  optionCodeMapping: any = {};
+
+  config: any = {
+    configXml: ""
+  }
+
+  optionKey: any = {
+    '%TEXT%': { element: 'input', style: { width: '100px' } },
+    '%TEXT1%': { element: 'input', style: { width: '30px' } },
+    '%TEXT2%': { element: 'input', style: { width: '60px' } },
+    '%TEXT3%': { element: 'input', style: { width: '100px' } },
+    '%TEXT4%': { element: 'input', style: { width: '150px' } },
+    '%TEXT5%': { element: 'input', style: { width: '300px' } },
+    '%TEXTAREA%': { element: 'textarea', style: { width: '100%' } }
+  };
+  constructor() { }
+
+  ngOnInit() {
+    $("#modal-editor").modal('show');
+  }
+
+  setConfig() {
+    if (this.config.configXml) {
+      try {
+        var template = xml2json.parser(this.config.configXml).Template || {};
+
+        template.QuestionSubject = [].concat(template.QuestionSubject || []);
+        template.QuestionSubject.forEach((subject) => {
+          subject.QuestionGroup = [].concat(subject.QuestionGroup || []);
+          subject.QuestionGroup.forEach(group => {
+            group.QuestionQuery = [].concat(group.QuestionQuery || []);
+            group.QuestionQuery.forEach(query => {
+              query.HasText = false;
+              query.ShowMark = false;
+
+              query.QuestionText = [].concat(query.QuestionText || []);
+              query.QuestionText.forEach(text => {
+                if (text.Text) {
+                  query.HasText = true;
+                }
+                text.Require = (text.Require == "true");
+                text.RequireLink = text.RequireLink || "";
+
+                text.ShowMark = false;
+
+                text.Option = [].concat(text.Option || []);
+                text.Option.forEach(option => {
+                  option.AnswerChecked = (option.AnswerChecked == "true");
+                  option.AnswerComplete = (option.AnswerComplete == "true");
+
+                  if (option.OptionCode) {
+                    this.optionCodeMapping[option.OptionCode] = option;
+                  }
+
+                  switch (text.Type) {
+                    case "單選":
+                      option.change = () => {
+                        if (!option.AnswerChecked) {
+                          option.AnswerChecked = true;
+                        }
+                        text.Option.forEach(optionC => {
+                          if (optionC != option) {
+                            optionC.AnswerChecked = false;
+                          }
+                        });
+                        this.refreshMark();
+                      };
+                      break;
+                    case "複選":
+                      option.change = () => {
+                        option.AnswerChecked = !option.AnswerChecked;
+                        this.refreshMark();
+                      };
+                      break;
+                    case "填答":
+                      option.AnswerChecked = true;
+                      break;
+                  }
+
+                  option.AnswerMatrix = [].concat(JSON.parse(option.AnswerMatrix || '[]') || []);
+                  option.IsTextArea = false;
+                  option.Template = [];
+                  //分割OptionText進Template
+                  var splitTemplate = () => {
+                    var keyWord = [];
+                    for (var key in this.optionKey) {
+                      keyWord.push(key);
+                    }
+                    keyWord.reverse();
+
+                    var keySplit = (query, keyWord) => {
+                      var key = keyWord.pop();
+                      var list = query.split(key);
+
+                      list.forEach((item, index) => {
+                        if (keyWord.length > 0) {
+                          if (item)
+                            keySplit(item, [].concat(keyWord));
+                        }
+                        else {
+                          if (item == "" && (index == 0 || index + 1 == list.length)) {
+
+                          }
+                          else {
+                            option.Template.push(item);
+                          }
+                        }
+                        if (index + 1 != list.length) {
+                          if (this.optionKey[key].element == 'textarea') { option.IsTextArea = true; }
+                          option.Template.push(key);
+                        }
+                      });
+                    }
+                    keySplit(option.OptionText, keyWord);
+                  };
+                  splitTemplate();
+                });
+              });
+            });
+          });
+        });
+        this.questionSubject = template.QuestionSubject;
+        this.refreshMark();
+        $("#modal-editor").modal('hide');
+      }
+      catch (exc) {
+        alert("ERROR：" + exc);
+      }
+    }
+  }
+  refreshMark() {
+    this.requireList = [];
+    this.questionSubject.forEach(subject => {
+      subject.QuestionGroup.forEach(group => {
+        group.QuestionQuery.forEach(query => {
+          query.ShowMark = false;
+          query.QuestionText.forEach(text => {
+            text.ShowMark = false;
+
+            var hasChecked = false;
+            var hasAllComplete = true;
+            text.Option.forEach(option => {
+              if (option.AnswerChecked) {
+                option.AnswerComplete = true;
+                hasChecked = true;
+                option.Template.forEach((templateItem, index) => {
+                  if (this.optionKey[templateItem]) {
+                    if (!option.AnswerMatrix[index]) {
+                      option.AnswerComplete = false;
+                      hasAllComplete = false;
+                    }
+                  }
+                  else {
+                    if (option.AnswerMatrix.length > index) {
+                      option.AnswerMatrix[index] = templateItem;
+                    }
+                    else {
+                      option.AnswerMatrix.push(templateItem);
+                    }
+                  }
+                });
+                option.AnswerValue = option.AnswerMatrix.join("");
+              }
+              else {
+                option.AnswerComplete = true;
+                option.AnswerValue = "";
+              }
+            });
+
+            if (
+              text.Require ||
+              (
+                text.RequireLink
+                && this.optionCodeMapping[text.RequireLink]
+                && this.optionCodeMapping[text.RequireLink].AnswerChecked
+              )
+            ) {
+              if (!hasChecked || !hasAllComplete) {
+                // this.requireCount++;
+                if (text.Text) {
+                  this.requireList.push(subject.Subject + "/" + group.Group + "/" + query.Query + "/" + text.Text);
+                  text.ShowMark = true;
+                }
+                else {
+                  this.requireList.push(subject.Subject + "/" + group.Group + "/" + query.Query);
+                  query.ShowMark = true;
+                }
+              }
+            }
+          });
+        });
+      });
+    });
+  }
+  showRequireList() {
+    alert(this.requireList.join("\n"));
+  }
+}
