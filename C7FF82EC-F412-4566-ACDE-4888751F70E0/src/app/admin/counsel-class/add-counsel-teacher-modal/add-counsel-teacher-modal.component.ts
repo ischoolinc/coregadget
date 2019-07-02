ae300186e9bbadc2d508fbacfbc0ee9f64193745
ChunTai Chen <chuntaichen@ChunTaide-MacBook-Pro.local>
2019-07-02 18:41:15 +0800

    輔導系統：加入管理輔導身分與設定輔導老師班級功能。

---
+++ C7FF82EC-F412-4566-ACDE-4888751F70E0/src/app/admin/counsel-class/add-counsel-teacher-modal/add-counsel-teacher-modal.component.ts
@@ -0,0 +1,147 @@
+import { Component, OnInit } from '@angular/core';
+import { CounselTeacherClass, CounselClass, GradeClassInfo } from "../counsel-class-vo";
+import { DsaService } from "../../../dsa.service";
+@Component({
+  selector: 'app-add-counsel-teacher-modal',
+  templateUrl: './add-counsel-teacher-modal.component.html',
+  styleUrls: ['./add-counsel-teacher-modal.component.css']
+})
+export class AddCounselTeacherModalComponent implements OnInit {
+
+  _CounselTeacherClass: CounselTeacherClass = new CounselTeacherClass();
+  tmpGradeYear: number[] = [];
+  tmpClass: CounselClass[] = [];
+  isSelectAllItem: boolean = false;
+  selectClassIDs: string[] = [];
+  SelectGradeYearList: GradeClassInfo[] = [];
+  constructor(private dsaService: DsaService) { }
+
+  ngOnInit() {
+
+    this.loadData();
+
+  }
+
+  loadData() {
+    this.isSelectAllItem = false;
+    this.GetCounselClass();
+  }
+
+  SetSelectGradeItem(gradeYear: number) {
+    this.SelectGradeYearList.forEach(item => {
+
+      if (item.GradeYear === gradeYear) {
+        item.Checked = !item.Checked;
+        item.ClassItems.forEach(classItem => {
+          classItem.Checked = item.Checked;
+        });
+      }
+    });
+  }
+
+  SetSelectAllItem() {
+    
+    this.isSelectAllItem = !this.isSelectAllItem;
+    this.SelectGradeYearList.forEach(item => {
+      item.Checked = this.isSelectAllItem;
+      item.ClassItems.forEach(classItem => {
+        classItem.Checked = this.isSelectAllItem;
+      });
+    });
+  }
+
+
+  // 取得教師輔導班級
+  async GetCounselClass() {
+    this.SelectGradeYearList = [];
+    this.tmpClass = [];
+    this.tmpGradeYear = [];
+    let resp = await this.dsaService.send("GetClasses", {
+      Request: {}
+    });
+
+    [].concat(resp.Class || []).forEach(counselClass => {
+      let gryear: number = parseInt(counselClass.GradeYear);
+      let CClass: CounselClass = new CounselClass();
+      CClass.GradeYear = gryear;
+      CClass.ClassName = counselClass.ClassName;
+      CClass.ClassID = counselClass.ClassID;
+      CClass.Checked = false;
+      this.tmpClass.push(CClass);
+      if (!this.tmpGradeYear.includes(gryear)) {
+        this.tmpGradeYear.push(gryear);
+      }
+    });
+
+
+
+    // 整理資料
+    this.tmpGradeYear.forEach(gr => {
+      let grClass: GradeClassInfo = new GradeClassInfo();
+      grClass.GradeYear = gr;
+      grClass.Checked = false;
+      grClass.ClassItems = this.tmpClass.filter(x => x.GradeYear === gr);
+      this.SelectGradeYearList.push(grClass);
+    });
+
+    this.filterCheckBox();
+  
+
+    //this.isLoading = false;
+  }
+
+  filterCheckBox() {
+    let className: string[] = this._CounselTeacherClass.ClassNames.split(',');
+
+        this.SelectGradeYearList.forEach(item => {
+          item.ClassItems.forEach(classItem => {
+         
+            classItem.Checked = className.includes(classItem.ClassName);
+            
+          });
+        });
+  }
+
+  save() {
+    this.selectClassIDs = [];    
+    this.SelectGradeYearList.forEach(item => {
+      item.ClassItems.forEach(classItem => {
+        if (classItem.Checked) {
+          this.selectClassIDs.push(classItem.ClassID);
+        }
+      });
+    });
+
+    if (this.selectClassIDs.length > 0) {
+      this.SetCounselClasses();
+    }
+  }
+
+  async SetCounselClasses() {
+
+    let reqClassIDs = [];
+    this.selectClassIDs.forEach(id => {
+
+      let itItm = {
+        ClassID: id
+      }
+      reqClassIDs.push(itItm);
+    });
+
+    let resp = await this.dsaService.send("SetCounselClasses", {
+      Request: {
+        TeacherID: this._CounselTeacherClass.TeacherID,
+        ClassIDs: reqClassIDs
+      }
+    });
+    console.log(resp);
+    $("#addCounselTeacher").modal("hide");
+  }
+
+
+  cancel() {
+    $("#addCounselTeacher").modal("hide");
+  }
+}
+
+
--- C7FF82EC-F412-4566-ACDE-4888751F70E0/src/app/admin/counsel-teacher-role/counsel-teacher-role.component.ts
+++ C7FF82EC-F412-4566-ACDE-4888751F70E0/src/app/admin/counsel-teacher-role/counsel-teacher-role.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, Optional } from '@angular/core';
+import { Component, OnInit, Optional, ViewChild } from '@angular/core';
 import { AdminComponent } from "../admin.component";
 import { ActivatedRoute, Router } from '@angular/router';
 import { DsaService } from "../../dsa.service";
+import { Alert } from 'selenium-webdriver';
+import { TeacherCounselRole } from "./counsel-teacher-role-vo";
+import { AddCounselTeacherRoleModalComponent } from "./add-counsel-teacher-role-modal/add-counsel-teacher-role-modal.component";
+import { DelCounselTeacherRoleModalComponent } from "./del-counsel-teacher-role-modal/del-counsel-teacher-role-modal.component";
 
 @Component({
   selector: 'app-counsel-teacher-role',
@@ -12,6 +16,7 @@ export class CounselTeacherRoleComponent implements OnInit {
 
   isLoading = false;
   teachersCounselRole: TeacherCounselRole[] = [];
+  notTeachersCounselRole: TeacherCounselRole[] = [];
   counselRole: string[] = [];
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -20,12 +25,15 @@ export class CounselTeacherRoleComponent implements OnInit {
     @Optional()
     private adminComponent: AdminComponent) { }
 
+
+  @ViewChild("addCounselTeacherRole") _addCounselTeacherRole: AddCounselTeacherRoleModalComponent;
+  @ViewChild("delCounselTeacherRole") _delCounselTeacherRole: DelCounselTeacherRoleModalComponent;
+
   ngOnInit() {
-    this.isLoading = true;
+
     this.adminComponent.currentItem = "counsel_teacher_role";
     this.counselRole = [];
-    // 目前輔導身分counselRole
-    this.counselRole.push('空白');
+    // 目前輔導身分counselRole    
     this.counselRole.push('輔導主任');
     this.counselRole.push('輔導組長');
     this.counselRole.push('專任輔導');
@@ -35,14 +43,34 @@ export class CounselTeacherRoleComponent implements OnInit {
     this.loadData();
   }
 
-  save() {
-    this.SetTeachersCounselRole();
+  Add() {
+    this._addCounselTeacherRole.counselRole = this.counselRole;
+    // 填入老師姓名
+    this._addCounselTeacherRole.options = [];
+    this._addCounselTeacherRole.selectTeacherID = "";
+    this._addCounselTeacherRole.myControl.setValue('');
+    this.notTeachersCounselRole.forEach(item => {
+      this._addCounselTeacherRole.options.push(item.TeacherName);
+    });
+
+    this._addCounselTeacherRole.selectRole = "請選擇身分";
+    this._addCounselTeacherRole.selectTeacherName = "請選擇教師";
+    this._addCounselTeacherRole.notTeachersCounselRole = this.notTeachersCounselRole;
+    $("#addCounselTeacherRole").modal("show");
+
+    // 關閉畫面
+    $("#addCounselTeacherRole").on("hide.bs.modal", () => {
+      // 重整資料
+      this.loadData();
+      $("#addCounselTeacherRole").off("hide.bs.modal");
+    });
   }
 
   loadData() {
+    this.isLoading = true;
     // 載入輔導身分
     this.GetTeachersCounselRole();
-    this.isLoading = false;
+
   }
 
   // 設定教師角色
@@ -55,9 +83,23 @@ export class CounselTeacherRoleComponent implements OnInit {
     teacher.isChage = true;
   }
 
+  delete(item: TeacherCounselRole) {
+
+    this._delCounselTeacherRole.teacherCounselRole = item;
+    $("#delCounselTeacherRole").modal("show");
+
+    // 關閉畫面
+    $("#delCounselTeacherRole").on("hide.bs.modal", () => {
+      // 重整資料
+      this.loadData();
+      $("#delCounselTeacherRole").off("hide.bs.modal");
+    });
+  }  
+
   // 取得所有教師輔導身分
   async GetTeachersCounselRole() {
     this.teachersCounselRole = [];
+    this.notTeachersCounselRole = [];
     let resp = await this.dsaService.send("GetTeachersCounselRole", {
       Request: {}
     });
@@ -67,35 +109,38 @@ export class CounselTeacherRoleComponent implements OnInit {
       tea.TeacherID = CounselRole.TeacherID;
       tea.TeacherName = CounselRole.TeacherName;
       tea.Role = CounselRole.Role;
-      this.teachersCounselRole.push(tea);
+      tea.parseOrder();
+      if (tea.Role !== '') {
+        this.teachersCounselRole.push(tea);
+      } else {
+        this.notTeachersCounselRole.push(tea);
+      }
     });
+
+    this.teachersCounselRole.sort(function (a, b) {
+      return a.Order - b.Order;
+    });
+    this.isLoading = false;
   }
 
   // 批次設定教師角色
   async SetTeachersCounselRole() {
     let reqTeacherCounselRole = [];
-    this.teachersCounselRole.forEach(tea =>{
-      if (tea.isChage)
-      {
+    this.teachersCounselRole.forEach(tea => {
+      if (tea.isChage) {
         let itItm = {
           TeacherID: tea.TeacherID,
           Role: tea.Role
         }
         reqTeacherCounselRole.push(itItm);
       }
-    }); 
-    let resp = await this.dsaService.send("SetTeachersCounselRole", {
-      Request: {TeacherCounselRole:reqTeacherCounselRole}
     });
- 
-    console.log(resp);   
+    let resp = await this.dsaService.send("SetTeachersCounselRole", {
+      Request: { TeacherCounselRole: reqTeacherCounselRole }
+    });
+
+    //console.log(resp);
   }
 }
 
-export class TeacherCounselRole {
-  constructor() { }
-  TeacherID: string;
-  Role: string;
-  TeacherName: string;
-  isChage: boolean = false;
-}
+
