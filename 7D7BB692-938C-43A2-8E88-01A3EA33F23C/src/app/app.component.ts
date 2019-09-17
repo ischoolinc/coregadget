974e025877e91f002b4112aa0e54435409c0a58a
ChunTai Chen <chuntaichen@ChunTaide-MacBook-Pro.local>
2019-09-17 17:24:57 +0800

    輔導學生：加入綜合記錄表選項與開始結束時間判斷。

--- 7D7BB692-938C-43A2-8E88-01A3EA33F23C/src/app/app.component.ts
+++ 7D7BB692-938C-43A2-8E88-01A3EA33F23C/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router, RoutesRecognized } from "@angular/router";
-
+import { FillSection } from "./section-vo";
 import { DsaService } from "./dsa.service";
 
 @Component({
@@ -9,40 +9,57 @@ import { DsaService } from "./dsa.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
- 
+  fillSectionList: FillSection[] = [];
   constructor(
     private activeRoute: ActivatedRoute,
     private router: Router,
     private dsaService: DsaService
   ) { }
 
-  async ngOnInit() {   
+  async ngOnInit() {
 
-    await this.RunCounselReportAUrl();
+    await this.GetFillSections();
   }
 
-  async RunCounselReportAUrl(){
-   // 取得學生身分 
-   let KeyID = '';
-   await this.dsaService.getSessionIDAndAccessPoint();
-   let StudentRsp = await this.dsaService.send("GetFillInTargetKeyID", {});
-   [].concat(StudentRsp.Student || []).forEach(Student => {
-     KeyID = Student.KeyID;
-   
-   });
-   
-   let dsns = gadget.getApplication().accessPoint;
+  async GetFillSections() {
+    try {
 
-   if (dsns && KeyID)   
-   {
-    let url = `https://web2.ischool.com.tw/deployment/C7FF82EC-F412-4566-ACDE-4888751F70E0/content.htm#/(simple-page:simple-page/${dsns}/comprehensive_fill/${KeyID})`;
-    window.open(url,'_self'); 
-   }else
-   {
-     alert("開啟失敗，無法解析代碼。");
-   }
-// 自己
-  //  window.open(url,'_balnk'); // new tab
+      let StudentRsp = await this.dsaService.send("GetFillSectionKeyID", {});
+      [].concat(StudentRsp.Section || []).forEach(Section => {
+        let sec: FillSection = new FillSection();
+        sec.SectionID = Section.SectionID;
+        sec.SchoolYear = Section.SchoolYear;
+        sec.Semester = Section.Semester;
+        sec.SectionName = Section.SectionName;
+        sec.KeyID = Section.KeyID;
+        sec.StartTime = Section.StartTime;
+        sec.EndTime = Section.EndTime;
+        sec.IsPassTime = Section.IsPassTime;
+        sec.keyButtonDisable = true;
+
+        if (sec.IsPassTime === 't' && sec.KeyID.length > 0)
+          sec.keyButtonDisable = false;
+
+        this.fillSectionList.push(sec);
+      });
+    } catch (err) {
+      alert("取得FillSections失敗(GetFillSections):" + err.dsaError.message);
+    }
+  }
+
+
+  async RunCounselReportAUrl(KeyID: string) {
+
+    let dsns = gadget.getApplication().accessPoint;
+
+    if (dsns && KeyID) {
+      let url = `https://web2.ischool.com.tw/deployment/C7FF82EC-F412-4566-ACDE-4888751F70E0/content.htm#/(simple-page:simple-page/${dsns}/comprehensive_fill/${KeyID})`;
+      window.open(url, '_balnk');
+    } else {
+      alert("開啟失敗，無法解析代碼。");
+    }
+    // 自己 _self
+    //  window.open(url,'_balnk'); // new tab
   }
 
 
