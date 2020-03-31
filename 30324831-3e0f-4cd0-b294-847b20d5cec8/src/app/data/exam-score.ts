3781680286c906d2959d1c9150ec4ebcf68719f6
grady <grady.liu@ischool.com.tw>
2020-03-31 17:27:15 +0800

    「班級歷年成績查詢」 中港高中專案。

---
+++ 30324831-3e0f-4cd0-b294-847b20d5cec8/src/app/data/exam-score.ts
@@ -0,0 +1,10 @@
+export interface ExamScoreRec {
+    student_id: number;
+    name: string;
+    seat_no: number;
+    subject: string;
+    sc_attend_id: string;
+    exam_id: number;
+    exam_name: string;
+    score: string;
+}
