65a0f17a897772185caab1ea7709b453d22ee08e
Elvira <elvira.chen@ischool.com.tw>
2020-06-03 18:18:22 +0800

    智慧簡訊(老師)  - 傳遞參數 &paramValues={course: 'enabled'} 開啟授課老師可以選擇課程學生功能。未開啟時只有班導師可以選擇班級學生。

--- 519299d9-7e15-4567-97d0-dd4f270842e2/src/app/chooser/chooser.component.ts
+++ 519299d9-7e15-4567-97d0-dd4f270842e2/src/app/chooser/chooser.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { TeacherService, ClassStudentRecord as ClassStudentRecordBase } from '../teacher.service';
-import { map, concatMap } from 'rxjs/operators';
+import {
+  TeacherService,
+  ClassStudentRecord as ClassStudentRecordBase,
+  CourseStudentRecord as CourseStudentRecordBase
+} from '../teacher.service';
+import { map, concatMap, filter } from 'rxjs/operators';
 import { MatDialogRef } from '@angular/material';
-import { SelectionResult, ClassSelection, StudentSelection } from './selection-result';
+import { SelectionResult, ClassSelection, StudentSelection, CourseSelection } from './selection-result';
 
 /** 內部使用。 */
+export interface ColRecord {
+  [x: string]: any;
+  students: ClassStudentRecord[];
+  checked?: boolean;
+}
+
 export interface ClassRecord {
   classId: string;
   className: string;
@@ -12,7 +22,19 @@ export interface ClassRecord {
   checked?: boolean;
 }
 
+export interface CourseRecord {
+  courseId: string;
+  courseName: string;
+  courseSubject: string;
+  students: CourseStudentRecord[];
+  checked?: boolean;
+}
+
 export interface ClassStudentRecord extends ClassStudentRecordBase {
+  checked?: boolean;
+}
+
+export interface CourseStudentRecord extends CourseStudentRecordBase {
   checked?: boolean;
 }
 
@@ -25,7 +47,8 @@ export class ChooserComponent implements OnInit {
 
   loading = false;
 
-  classes: ClassRecord[] = [];
+  classes: ColRecord | ClassRecord[] = [];
+  courses: ColRecord | CourseRecord[] = [];
 
   selectionCount = 0;
 
@@ -35,19 +58,33 @@ export class ChooserComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
-
     try {
       this.loading = true;
       this.classes = await this.teacher.getMyClass().pipe(
         // 轉成 class id 陣列。
-        map(objClasses => {
-          return objClasses.map(objClass => objClass.ClassID);
-        }),
+        map(objClasses => objClasses.map(objClass => objClass.ClassID)),
+        // 如果無課程就不往下執行
+        filter(cls => cls.length ? true : false),
         // 呼叫取得班級學生清單。
         concatMap(cls => this.teacher.getClassStudentsV2(cls)),
         // 轉換成 ClassRecord 格式。
-        map(students => this.students2ClassRecords(students))
-      ).toPromise();
+        map(students => this.students2ClassRecords(students)),
+      ).toPromise() || [];
+
+      if (gadget.params.course === 'enabled') {
+        this.courses = await this.teacher.getCurrentSemester().pipe(
+          // 轉成 學年期物件。
+          concatMap(objSemester => this.teacher.getMyCourses(objSemester.SchoolYear, objSemester.Semester)),
+          // 轉成 course id 陣列。
+          map(objCourses => objCourses.map(objCourse => objCourse.CourseID)),
+          // 如果無課程就不往下執行
+          filter(courses => courses.length ? true : false),
+          // 呼叫取得課程學生清單。
+          concatMap(courses => courses.length ? this.teacher.getCourseStudentV2(courses) : []),
+          // 轉換成 CourseRecord 格式。
+          map(students => this.students2CourseRecords(students))
+        ).toPromise() || [];
+      }
     } catch (err) {
       console.log(err);
     } finally {
@@ -75,16 +112,36 @@ export class ChooserComponent implements OnInit {
     return Array.from(clsmap.values());
   }
 
-  toggleAll(cls: ClassRecord) {
-    cls.students.forEach(stu => {
-      stu.checked = cls.checked;
+  private students2CourseRecords(students: CourseStudentRecord[]) {
+    const coursemap = new Map<string, CourseRecord>();
+
+    for (const student of students) {
+      const { CourseID, CourseName, CourseSubject } = student;
+
+      if (!!!coursemap.get(CourseID)) {
+        coursemap.set(CourseID, {
+          courseId: CourseID,
+          courseName: CourseName,
+          courseSubject: CourseSubject,
+          students: []
+        });
+      }
+
+      coursemap.get(CourseID).students.push(student);
+    }
+    return Array.from(coursemap.values());
+  }
+
+  toggleAll(item: ColRecord) {
+    item.students.forEach(stu => {
+      stu.checked = item.checked;
     });
 
     this.sumSelectionCount();
   }
 
-  reflectClassSelection(cls: ClassRecord) {
-    cls.checked = cls.students
+  reflectColSelection(item: ColRecord) {
+    item.checked = item.students
       .map(v => v.checked)
       .reduce((acc, cur) => acc && cur);
 
@@ -94,6 +151,13 @@ export class ChooserComponent implements OnInit {
   sumSelectionCount() {
     let total = 0;
     for (const cls of this.classes) {
+      total += cls.students
+        .map(stu => stu.checked)
+        .filter(selected => selected)
+        .length;
+    }
+
+    for (const cls of this.courses) {
       total += cls.students
         .map(stu => stu.checked)
         .filter(selected => selected)
@@ -110,6 +174,20 @@ export class ChooserComponent implements OnInit {
 
       if (!!cr.checked) {
         selections.push(new ClassSelection(cr));
+        continue;
+      }
+
+      for (const stud of cr.students) {
+        if (!!stud.checked) {
+          selections.push(new StudentSelection(stud));
+        }
+      }
+    }
+
+    for (const cr of this.courses) {
+
+      if (!!cr.checked) {
+        selections.push(new CourseSelection(cr));
         continue;
       }
 
