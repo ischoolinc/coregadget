1fbd4e49c007d6d3bc913266955c67bbc184c36e
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2020-12-02 14:22:54 +0800

    完整的選擇學生功能展示。

--- 082711DA-B5D5-40D1-AF34-D8BEBE246CA1/src/app/chooser/demo/demo.component.ts
+++ 082711DA-B5D5-40D1-AF34-D8BEBE246CA1/src/app/chooser/demo/demo.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription, Subject } from 'rxjs';
 import { ChooserComponent } from '../chooser.component';
 import { SelectionResult } from '../data';
+import { ReceiversService } from '../receivers.service';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, OnDestroy {
+
+  items: SelectionResult[];
+
+  private destroy$ = new Subject<void>();
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private receiver: ReceiversService
   ) { }
 
   ngOnInit(): void {
+    this.receiver.receivers$.pipe(
+      takeUntil(this.destroy$),
+    )
+    .subscribe(v => {
+      this.items = v;
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 
   openChooser() {
