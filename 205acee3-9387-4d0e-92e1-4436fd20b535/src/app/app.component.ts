bf681f520b416117b7c3e40e57c0224cf2731f48
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-01-07 18:41:13 +0800

    引用一些會用到的 module。

--- 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app.component.ts
+++ 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  notes: Section[] = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: '107'},
+    {value: 'pizza-1', viewValue: '108'},
+    {value: 'tacos-2', viewValue: '109'}
+  ];
+
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
--- 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app.module.ts
+++ 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatTabsModule,
+    MatListModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
