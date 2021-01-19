d3bbd05e61097be42db223d1eb0209b35607fbae
grady <grady0902@gmail.com>
2021-01-19 15:42:51 +0800

    「課程規劃」設定畫面新增路由守衛，確認目前資料狀態。

--- 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app-routing.module.ts
+++ 205acee3-9387-4d0e-92e1-4436fd20b535/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanGuard } from './core/plan.guard';
 import { PlanComponent } from './plan/plan.component';
 
 const routes: Routes = [
-  { path: 'plan/:id', component: PlanComponent}
+  { path: 'plan/:id', component: PlanComponent, canActivate: [PlanGuard]}
 ];
 
 @NgModule({
