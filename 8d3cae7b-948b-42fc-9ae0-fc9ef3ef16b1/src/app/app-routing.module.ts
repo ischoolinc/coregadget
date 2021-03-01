d3b739dea5092a5b3fd43fd85d5a292a02f269d8
Kevin Huang <kevin.huang@ischool.com.tw>
2021-03-01 13:34:33 +0800

    刪掉不必要的程式碼。

--- 8d3cae7b-948b-42fc-9ae0-fc9ef3ef16b1/src/app/app-routing.module.ts
+++ 8d3cae7b-948b-42fc-9ae0-fc9ef3ef16b1/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { ClassSummaryComponent } from './ui/class-summary/class-summary.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StudentDetailComponent } from './ui/student-detail/student-detail.component';
 
 const routes: Routes = [
-  {path: '', component: ClassSummaryComponent},
-  {path: 'student-detail', component: StudentDetailComponent}
+  {path: '', component: ClassSummaryComponent}
 ];
 
 @NgModule({
