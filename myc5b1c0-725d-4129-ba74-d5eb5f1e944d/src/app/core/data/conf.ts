08e7b1d35d9457c70c0870c083f88de593453201
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-08-27 17:49:03 +0800

    增加 conf Service。

---
+++ myc5b1c0-725d-4129-ba74-d5eb5f1e944d/src/app/core/data/conf.ts
@@ -0,0 +1,9 @@
+
+export interface CourseConf {
+    uid:        string;
+    course_id:  string;
+    service_id: string;
+    conf:       any;
+    link:       string;
+    enabled:    boolean;
+}
