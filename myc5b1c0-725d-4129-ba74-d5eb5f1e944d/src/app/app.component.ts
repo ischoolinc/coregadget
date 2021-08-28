d482156acc3d46975057c34c3954c134244cfb03
Elvira <elvira.chen@ischool.com.tw>
2021-08-28 10:38:39 +0800

    刪除不再使用的程式碼

--- myc5b1c0-725d-4129-ba74-d5eb5f1e944d/src/app/app.component.ts
+++ myc5b1c0-725d-4129-ba74-d5eb5f1e944d/src/app/app.component.ts
@@ -40,13 +40,8 @@ export class AppComponent implements OnInit {
   saving = false;
   dialogRef?: MatDialogRef<any>;
   dialogRefManage?: MatDialogRef<any>;
-  dialogRefAsync?: MatDialogRef<any>;
 
   errorMsg = '';
-  processState = 101;
-  processErrorMsg = '';
-  progressBar = { max: 0, current: 0 };
-  createStudentMsg = '';
 
   curSelectedContext: SelectedContext = {} as SelectedContext;
   dsns = '';
@@ -54,13 +49,10 @@ export class AppComponent implements OnInit {
   adminConnectedGoogle: { success: boolean, message: string } = { success: false, message: '' };
   curSemester: Semester = {} as Semester;
   courses: MyCourseRec[] = [];
-  googleCourses: GoogleClassroomCourse[] = [];
 
   #updateTimestamp = dj();
   classroomUpdateRequired = false;
   classroomUpdating = false;
-  googleClassroomLink = new FormControl(''); // 使用者提供的 Google Classroom Link。
-  dislinkingGoogle = false
   #classroom_url = 'https://oha.1campus.net';
 
   semesterRowSource: Semester[] = [];
@@ -80,8 +72,6 @@ export class AppComponent implements OnInit {
   manageProcessState = 301;
   manageProcessErrorMsg = '';
 
-  curCSTab = 'editlink';
-
   @ViewChild('semester', { static: true }) semester: SelectComponent = {} as SelectComponent;
 
   constructor(
@@ -92,7 +82,6 @@ export class AppComponent implements OnInit {
     private gClassroomSrv: GoogleClassroomService,
     private dialog: MatDialog,
     private snackbarSrv: SnackbarService,
-    private changeDetector: ChangeDetectorRef,
     private ConnectedSettingSrv: ConnectedSettingService,
     private crSrv: ClassroomService,
     private store: Store,
@@ -124,6 +113,7 @@ export class AppComponent implements OnInit {
     try {
       const info = this.store.selectSnapshot(ContextState.personalInfo);
       this.myInfo = info;
+      console.log(info);
 
       this.curSelectedContext = await this.login.getSelectedContext().toPromise() as SelectedContext;
       this.dsns = this.curSelectedContext.dsns;
@@ -284,307 +274,14 @@ export class AppComponent implements OnInit {
   manageService(target: MyCourseRec, template: TemplateRef<any>) {
     this.manageProcessErrorMsg = '';
     this.manageProcessState = 301;
-    this.processErrorMsg = '';
-    this.processState = 101;
     this.dialogRefManage = this.dialog.open(template, {
       data: { target },
       width: '480px',
     });
   }
 
-  asyncService(course: MyCourseRec, template: TemplateRef<any>) {
-    this.processErrorMsg = '';
-    this.processState = 201;
-    this.dialogRef = this.dialog.open(template, {
-      data: { course },
-      width: '480px',
-    });
-  }
-
-  async asyncGoogleClassroomCourse(course: MyCourseRec, action: 'create' | 'update' | 'link') {
-    if (!this.adminConnectedGoogle.success) { return; }
-    if ([102, 202].indexOf(this.processState) !== -1) { return; }
-
-    // 1. 取得共同授課老教師清單、取得課程學生清單
-    // 2. 建立課程 + 將老師加入課程(協同教學)
-    // 3. 將學生加入課程
-    // 4. 將 Google Classroom 課程資訊補充到 course
-
-    this.processErrorMsg = '';
-    this.processState = (
-      action === 'create' ? 102 :
-        action === 'update' ? 202 : 103);
-
-    if(action === 'link') {
-      this.progressBar = {
-        max: 100, // 一個步驟 => 50
-        current: 0,
-      };
-      await this.linkExistsGoogleClassroom(course);
-      return
-    }
-
-    this.progressBar = {
-      max: 50 * 4, // 一個步驟 => 50
-      current: 0,
-    };
-    this.createStudentMsg = '';
-
-    let newGCourse: GoogleClassroomCourse = {} as GoogleClassroomCourse;
-    let teachers: MyCourseTeacherRec[] = [];
-    let courseOwner: MyCourseTeacherRec = {} as MyCourseTeacherRec;
-    let coTeachers: MyCourseTeacherRec[] = [];
-    let students: any = [];
-
-    try { // 從 DSA 取得老師、學生清單。
-      const promiseList = [
-        this.myCourseSrv.getCoursetTeachers(this.dsns, course.CourseId),
-        this.myCourseSrv.getCourseStudents(this.dsns, course.CourseId)
-      ];
-
-      teachers = await promiseList[0];
-      courseOwner = teachers.find(v => v.TeacherSequence === 1) || {} as MyCourseTeacherRec;
-      coTeachers = teachers.filter(v => v.TeacherSequence !== 1);
-
-      students = await promiseList[1];
-      this.progressBar.max += students.length;
-    } catch (error) {
-      this.processState = 9999;
-      this.processErrorMsg = '哎呀！取得班級資料時出了一點問題！';
-    }
-
-    // 開始同步
-    try {
-      this.reportProgress(50); // 取得資料完成
-
-      // 建立 Google Classroom.
-      if (action === 'create') {
-        newGCourse = await this.gClassroomSrv.createCourse(this.dsns, 'google_classroom_admin', {
-          id: course.Alias || '',
-          name: course.CourseName,
-          ownerId: courseOwner.LinkAccount,
-          courseState: 'ACTIVE',
-        });
-        // console.log(newGCourse);
-
-        await this.addTeacherToGoogleClassroomCourse(coTeachers, course.Alias || '');
-        this.reportProgress(50);
-      }
-
-      try { // 同步學生清單到 Google Classroom.
-        await this.addStudentToGoogleClassroomCourse(students, course.Alias || '');
-
-        if (action === 'create') { course.GoogleExt = newGCourse; }
-
-        this.progressBar.current = this.progressBar.max;
-        this.changeDetector.detectChanges();
-
-        setTimeout(() => {
-          this.processState = 2000;
-        }, 100);
-      } catch (error) {
-        this.processState = 9999;
-        this.processErrorMsg = '哎呀！班級加入學生時出了一點問題！';
-      }
-    } catch (error) {
-      this.processState = 9999;
-
-      if (error && error.error && error.error.status) {
-        if (error.error.status === 'ALREADY_EXISTS') {
-          this.processErrorMsg = '課程已存在！';
-        } else if (error.error.status === 'PERMISSION_DENIED') {
-          this.processErrorMsg = `教師帳號必須是有效的 Google 教育帳號！${courseOwner.LinkAccount}`;
-        }
-      }
-    }
-  }
-
-  async linkExistsGoogleClassroom(course: MyCourseRec) {
-    const { dsns, myInfo: { account } } = this;
-    const userLink = this.googleClassroomLink.value;
-    const userCode = this.getCodeFromClassroomUrl(userLink);
-    this.googleClassroomLink.setValue('');
-
-    try {
-      const courses = await this.gClassroomSrv.getCourses(dsns, 'google_classroom_admin', account);
-      this.reportProgress(50);
-      console.log(courses);
-
-      let found: GoogleClassroomCourse | null = null;
-      for(const course of courses) {
-        const { alternateLink } = course;
-        const courseCode = this.getCodeFromClassroomUrl(alternateLink);
-        if(userCode == courseCode) {
-          found = course;
-          break;
-        }
-      }
-
-      if(!found) {
-        this.processState = 9999;
-        this.processErrorMsg = `連結錯誤，此 Google Classroom 不存在、已封存或無權限存取。`;
-        return;
-      }
-
-      const rsp = await this.gClassroomSrv.setCourseAlias(dsns,
-        'google_classroom_admin',
-        found.id,
-        course.Alias || '');
-
-      course.GoogleExt = found;
-
-      this.processState = 2100;
-      this.createStudentMsg = `已將你的課程「${course.CourseName}」連結到 Google Classroom 的「${found.name}」。`;
-
-    } catch(err) {
-      this.processState = 9999;
-      this.processErrorMsg = `哎呀！連結 Google Classroom 發生錯誤！`;
-    }
-  }
-
-  async dislinkGoogleClassroom(course: MyCourseRec) {
-    const { dsns } = this;
-    const { GoogleExt } = course;
-
-    try {
-      this.dislinkingGoogle = true;
-      await this.gClassroomSrv.delCourseAlias(dsns,
-        'google_classroom_admin',
-        GoogleExt?.id || '',
-        course.Alias || '');
-
-      course.GoogleExt = undefined;
-    } catch(err) {
-      console.error(err);
-    } finally {
-      this.dislinkingGoogle = false
-    }
-  }
-
-  private getCodeFromClassroomUrl(url: string) {
-    const pattern = /https:\/\/classroom.google.com(\/\w)?(\/\d)?(\/\w)?\/([\w\s]*)/im;
-    const match = pattern.exec(url);
-    return match ? match[4] : null;
-  }
-
-  async addTeacherToGoogleClassroomCourse(teachers: MyCourseTeacherRec[], alias: string) {
-    const promiseList: { p: Promise<any>, teacher: any }[] = teachers.map(teacher => {
-      if (teacher.LinkAccount) {
-        return {
-          p: this.gClassroomSrv.createTeacher(this.dsns, 'google_classroom_admin', alias, teacher.LinkAccount),
-          teacher: teacher,
-        };
-      } else {
-        return { p: new Promise((r, j) => j('skip')), teacher: teacher };
-      }
-    });
-
-    // let createTeacherSuccesCount = 0;
-    // const createTeacherErrors = [];
-
-    for (const item of promiseList) {
-      try {
-        await item.p;
-        // createTeacherSuccesCount += 1;
-      } catch (error) {
-        // if (error && error.error && error.error.status) {
-        //   if (error.error.status !== 'ALREADY_EXISTS') {
-        //     createTeacherErrors.push(`${item.teacher.TeacherName} (${item.teacher.LinkAccount})`);
-        //   }
-        // } else {
-        //   createTeacherErrors.push(`${item.teacher.TeacherName} (${item.teacher.LinkAccount})`);
-        // }
-      } finally {
-        // this.createTeacherMsg = `成功建立協同教師數：${createTeacherSuccesCount}`;
-      }
-    }
-  }
-
-  async addStudentToGoogleClassroomCourse(students: any[], alias: string) {
-    const promiseList: { p: Promise<any>, student: any }[] = students.map(student => {
-      if (student.LinkAccount) {
-        return {
-          p: this.gClassroomSrv.createStudent(this.dsns, 'google_classroom_admin', alias, student.LinkAccount),
-          student,
-        };
-      } else {
-        return { p: new Promise((r, j) => j('skip')), student };
-      }
-    });
-
-    let createStudentSuccesCount = 0;
-    // const createStudentErrors = [];
-
-    for (const item of promiseList) {
-      try {
-        await item.p;
-        createStudentSuccesCount += 1;
-        this.reportProgress(50 / promiseList.length);
-      } catch (error) {
-        // if (error && error.error && error.error.status) {
-        //   if (error.error.status !== 'ALREADY_EXISTS') {
-        //     createStudentErrors.push(`${item.student.StudentName} (${item.student.LinkAccount})`);
-        //   }
-        // } else {
-        //   createStudentErrors.push(`${item.student.StudentName} (${item.student.LinkAccount})`);
-        // }
-      } finally {
-        this.createStudentMsg = `成功建立學生成員數：${createStudentSuccesCount}`;
-      }
-    }
-
-    if (promiseList.length === 0) { this.reportProgress(50); }
-  }
-
-  // toggleGoogleClassroomCourse(e: MatSlideToggleChange, course: MyCourseRec) {
-  //   if (e.checked) {
-  //     this.enabledGoogleClassroomCourse(course);
-  //   } else {
-  //     this.disabledGoogleClassroomCourse(course);
-  //   }
-  // }
-
-  // async enabledGoogleClassroomCourse(course: MyCourseRec) {
-  //   if (this.saving) { return; }
-
-  //   try {
-  //     this.saving = true;
-  //     const rsp = await this.patchGoogleClassroomCourse(course, 'ACTIVE');
-  //     course.GoogleExt = rsp;
-  //   } catch (error) {
-  //     this.snackbarSrv.show('啟用 Google Classroom 發生錯誤！');
-  //   } finally {
-  //     this.saving = false;
-  //   }
-  // }
-
-  // async disabledGoogleClassroomCourse(course: MyCourseRec) {
-  //   if (this.saving) { return; }
-
-  //   try {
-  //     this.saving = true;
-  //     const rsp = await this.patchGoogleClassroomCourse(course, 'ARCHIVED'); // 封存課程
-  //     course.GoogleExt = rsp;
-  //   } catch (error) {
-  //     this.snackbarSrv.show('關閉 Google Classroom 發生錯誤！');
-  //   } finally {
-  //     this.saving = false;
-  //   }
-  // }
-
-  async patchGoogleClassroomCourse(course: MyCourseRec, courseState: GoogleCourseState) {
-    return this.gClassroomSrv.patchCourse(this.dsns, 'google_classroom_admin', course.Alias || '', {
-      courseState
-    });
-  }
-
   googleSigninChooserUrl(url: string) {
     return `https://accounts.google.com/ServiceLogin/signinchooser?service=accountsettings&continue=${url}`;
-  }
-
-  reportProgress(value: number) {
-    this.progressBar = { ...this.progressBar, current: this.progressBar.current += value };
-    this.changeDetector.detectChanges();
   }
 
   classroomUrl(course: MyCourseRec) {
@@ -632,12 +329,6 @@ export class AppComponent implements OnInit {
     } catch (error) {
       target.SystemCloudService.get(service)!.Enabled = !e.checked;
     }
-  }
-
-  openAddCloudService() {
-  }
-
-  openEditCloudService() {
   }
 
   openClassTime() {
