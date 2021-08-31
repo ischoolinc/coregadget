022341a2d7e0c367af91fc950bf0e26c5789316b
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-08-31 17:30:39 +0800

    新增 setConf store。

--- myc5b1c0-725d-4129-ba74-d5eb5f1e944d/src/app/core/core.module.ts
+++ myc5b1c0-725d-4129-ba74-d5eb5f1e944d/src/app/core/core.module.ts
@@ -4,14 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxsModule } from '@ngxs/store';
 import { ContextState } from './states/context.state';
 import { TimetableState } from './states/timetable.state';
-import { CourseConfState } from './states/conf.state';
+import { ServiceConfState } from './states/conf.state';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
-    NgxsModule.forFeature([ContextState, TimetableState, CourseConfState])
+    NgxsModule.forFeature([ContextState, TimetableState, ServiceConfState])
   ]
 })
 export class CoreModule { }
