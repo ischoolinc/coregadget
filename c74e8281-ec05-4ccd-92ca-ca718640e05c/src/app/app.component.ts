57bcb64bd5bd679004adc5d13904c081bf4904fe
黃俊威 <dylan.huang@ischool.com.tw>
2021-12-13 17:38:21 +0800

    修改新增 判斷老師加減分上下享限的功能

--- c74e8281-ec05-4ccd-92ca-ca718640e05c/src/app/app.component.ts
+++ c74e8281-ec05-4ccd-92ca-ca718640e05c/src/app/app.component.ts
@@ -14,9 +14,13 @@ export class AppComponent implements OnInit {
   endTime: string; //加減分填寫結束時間
   curClass: ClassRec; //目前所選班級
 
-  currentSchoolYear:string ; //預設學年度
-  currentSemester:string; //預設學期
-  currSchoolYearName:string; //學年期名稱 
+
+  currentSchoolYear: string; //預設學年度
+  currentSemester: string; //預設學期
+  currSchoolYearName: string; //學年期名稱 
+
+  Range: Number
+  LowRange: Number
 
   isLoading: boolean; //載入中
   contract: any; //資料服務
@@ -27,13 +31,13 @@ export class AppComponent implements OnInit {
   moralScoreList: MoralScoreRec[];
   curStudent: StudentRec;
 
-  regExp = /^[0-9]?$|^100$|^0$/;
-
+  //regExp = /^[0-9]?$|^100$|^0$/;
+  regExp = /^-?[1-9][0-9]?$|^100$|^0$/;
   modalRef: BsModalRef;
 
-  @ViewChild('tplError', {static: false}) tplError: TemplateRef<any>;
-  @ViewChild('tplEditForm', {static: false}) tplEditForm: TemplateRef<any>;
-  @ViewChild('tplForbid', {static: false}) tplForbid: TemplateRef<any>;
+  @ViewChild('tplError', { static: false }) tplError: TemplateRef<any>;
+  @ViewChild('tplEditForm', { static: false }) tplEditForm: TemplateRef<any>;
+  @ViewChild('tplForbid', { static: false }) tplForbid: TemplateRef<any>;
 
   constructor(
     private gadget: GadgetService,
@@ -47,49 +51,70 @@ export class AppComponent implements OnInit {
       this.contract = await this.gadget.getContract('ischool.moralconduct');
 
       await this.getDeadline();
+      await this.getConfig();
       await this.getDefSchoolyear();
       await this.getMyClass();
-      await this.getClassStudent();
-      await this.getMoralScore();
+
+      await this.reload();
 
 
-      this.currSchoolYearName = this.currentSchoolYear +"學年度 第" +this.currentSemester +"學期 班級德行加減分"
+      this.currSchoolYearName = this.currentSchoolYear + "學年度 第" + this.currentSemester + "學期 班級德行加減分"
     } catch (error) {
       console.log(error);
-    } finally{
+    } finally {
       this.isLoading = false;
     }
   }
 
+  async reload() {
+    await this.getClassStudent();
+    await this.getMoralScore();
+
+  }
+
+  async getConfig() {
+    try {
+      const rsp = await this.contract.send('GetConfig', {});
+      Apprais: Node;
+      const Apprais = rsp.Result.MoralConductScoreCalcRule.TeacherAppraise
+
+      this.Range = parseInt(Apprais["Range"]); //正值上限
+      this.LowRange = ~(parseInt(Apprais["Range"])) +1; //負數上限
+
+    } catch (error) {
+      alert(`Service GetDeadline: ${error}`)
+    }
+  }
+
   async getDeadline() {
-      try {
-        const rsp = await this.contract.send('GetDeadline', {});
+    try {
+      const rsp = await this.contract.send('GetDeadline', {});
 
-        if (rsp.Response.StartTime && rsp.Response.EndTime) {
-          this.startTime = moment(rsp.Response.StartTime).format('YYYY/MM/DD HH:mm');
-          this.endTime = moment(rsp.Response.EndTime).format('YYYY/MM/DD HH:mm');
-          this.isBeforeTheDeadline = moment(Date.now()).isBefore(this.endTime);
-        } else {
-          this.startTime = '未設定開始時間！';
-          this.endTime = '未設定結束時間！';
-          this.isBeforeTheDeadline = false;
-        }
-
-      } catch (error) {
-        alert(`Service GetDeadline: ${error}`)
+      if (rsp.Response.StartTime && rsp.Response.EndTime) {
+        this.startTime = moment(rsp.Response.StartTime).format('YYYY/MM/DD HH:mm');
+        this.endTime = moment(rsp.Response.EndTime).format('YYYY/MM/DD HH:mm');
+        this.isBeforeTheDeadline = moment(Date.now()).isBefore(this.endTime);
+      } else {
+        this.startTime = '未設定開始時間！';
+        this.endTime = '未設定結束時間！';
+        this.isBeforeTheDeadline = false;
       }
+
+    } catch (error) {
+      alert(`Service GetDeadline: ${error}`)
+    }
   }
 
   async getDefSchoolyear() {
 
-      try {
-        const rsp = await this.contract.send('GetDefSchoolYear', {});
-          this.currentSchoolYear = rsp.Result.SystemConfig.DefaultSchoolYear;
-          this.currentSemester = rsp.Result.SystemConfig.DefaultSemester;
+    try {
+      const rsp = await this.contract.send('GetDefSchoolYear', {});
+      this.currentSchoolYear = rsp.Result.SystemConfig.DefaultSchoolYear;
+      this.currentSemester = rsp.Result.SystemConfig.DefaultSemester;
 
-      } catch (error) {
-        alert(`Service GetDeadline: ${error}`)
-      }
+    } catch (error) {
+      alert(`Service GetDeadline: ${error}`)
+    }
   }
 
   async getMyClass() {
@@ -97,7 +122,7 @@ export class AppComponent implements OnInit {
       const rsp = await this.contract.send('GetMyClass', {});
       // 存取學年度學期
       this.classList = [].concat(rsp.Class || []);
-      
+
       if (this.classList.length > 0) {
         this.curClass = this.classList[0];
       } else {
@@ -111,17 +136,18 @@ export class AppComponent implements OnInit {
 
 
   async getClassStudent() {
-    if (!this.curClass.id)
-    {
+    if (!this.curClass.id) {
+
       return;
     } else {
+      //debugger;
       try {
-        const rsp = await this.contract.send('GetClassStudent', 
-        {
-          Condition:{
-            ClassID: this.curClass.id
-          }
-        });
+        const rsp = await this.contract.send('GetClassStudent',
+          {
+            Condition: {
+              ClassID: this.curClass.id
+            }
+          });
         this.studentList = [].concat(rsp.Response.Student || [])
       } catch (error) {
         alert(`Service GetClassStudent: ${error}`);
@@ -130,39 +156,39 @@ export class AppComponent implements OnInit {
   }
 
   async getMoralScore() {
-    if (!this.curClass.id)
-    {
+    if (!this.curClass.id) {
       return;
     } else {
       try {
-        const rsp = await this.contract.send('GetMoralAddScore', 
-        {
-          Request:{
-          Condition:{
-            SchoolYear: this.currentSchoolYear,
-            Semester: this.currentSemester,
-            ClassID: this.curClass.id
-          }
-        }});
+        const rsp = await this.contract.send('GetMoralAddScore',
+          {
+            Request: {
+              Condition: {
+                SchoolYear: this.currentSchoolYear,
+                Semester: this.currentSemester,
+                ClassID: this.curClass.id
+              }
+            }
+          });
         this.moralScoreList = [].concat(rsp.MoralScore.SbDiff || [])
-        console.log("moralScore:",this.moralScoreList);
+        console.log("moralScore:", this.moralScoreList);
 
         this.studentList.forEach(student => {
 
-          let nowMoral = this.moralScoreList.find(moralInfo => moralInfo.StudentID==student.StudentID);
-          if(nowMoral)
-          {
+          let nowMoral = this.moralScoreList.find(moralInfo => moralInfo.StudentID == student.StudentID);
+          if (nowMoral) {
+            student.MoralScoreID = nowMoral.MoralScoreID;
             student.SbDiff = nowMoral.SbDiff;
-            student.Comment = nowMoral.Comment;
-            student.OtherDiff = nowMoral.OtherDiff;
-            student.TextScore = nowMoral.TextScore;
-            student.InitialSummary = nowMoral.InitialSummary;
-            student.Summary = nowMoral.Summary;
+            // student.Comment = nowMoral.Comment;
+            // student.OtherDiff = nowMoral.OtherDiff;
+            // student.TextScore = nowMoral.TextScore;
+            // student.InitialSummary = nowMoral.InitialSummary;
+            // student.Summary = nowMoral.Summary;
           }
 
 
-        }); 
-      
+        });
+
 
       } catch (error) {
         alert(`Service GetMoralAddScore: ${error}`);
@@ -177,19 +203,45 @@ export class AppComponent implements OnInit {
 
   openEditForm(stu: StudentRec) {
     if (this.isBeforeTheDeadline) {
-      this.curStudent = {...stu};
-      this.modalRef = this.bsModalSrv.show(this.tplEditForm);  
+      this.curStudent = { ...stu };
+      this.modalRef = this.bsModalSrv.show(this.tplEditForm);
     } else {
       this.modalRef = this.bsModalSrv.show(this.tplForbid);
     }
-    
+
   }
 
-  validateMs() {
-      this.curStudent.validate_ms = this.regExp.test('' + this.curStudent.validate_ms);
+  /**  驗證數字是否可通過   */
+  validatePass(now_input: string): boolean {
+    //alert("驗證");
+    debugger;
+    this.curStudent.validate_ms = this.regExp.test('' + now_input);
+
+    if (this.curStudent.validate_ms) {
+
+      let input_number = parseInt(now_input);
+
+      if ((input_number <= this.Range) && (input_number >= this.LowRange)) {
+        //大於正數上限
+        this.curStudent.validate_ms = true;
+      } else {
+        //輸入數字正常時,檢查是否符合規範
+        this.curStudent.validate_ms = false;
+      }
+
+    }
+    return this.curStudent.validate_ms;
+
   }
 
   async save() {
+
+    if(!this.validatePass(this.curStudent.SbDiff))
+    {
+      alert(`輸入加減分不可大於${this.Range}/小於${this.LowRange}`);
+      return;
+    }
+
     if (this.curStudent) {
       this.isLoading = true;
       try {
@@ -197,20 +249,12 @@ export class AppComponent implements OnInit {
           ref_student_id: this.curStudent.StudentID,
           school_year: this.currentSchoolYear,
           semester: this.currentSemester,
-
-          //其他資料
           moral_score_id: this.curStudent.MoralScoreID,
-
-          sb_diff:this.curStudent.SbDiff,
-          other_diff: this.curStudent.OtherDiff,
-          text_score: this.curStudent.TextScore,
-          comment: this.curStudent.Comment,
-          initial_summary: this.curStudent.InitialSummary,
-          summary: this.curStudent.Summary,
+          sb_diff: this.curStudent.SbDiff,
         };
         const rsp = await this.contract.send('SetMoralAddScore', body)
-        
-        this.getClassStudent();
+
+        await this.reload();
         this.modalRef.hide();
 
       } catch (error) {
@@ -227,35 +271,35 @@ export class AppComponent implements OnInit {
     var downloadLink;
     var dataType = 'application/octet-stream';
     var tableSelect = document.querySelector(selector);
-    var tableHTML = encodeURIComponent(`  <meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'/><style> table, td {border:1px solid #dee2e6;  text-align: center;} table {border-collapse:collapse}</style>`+
-    `導師加減分-${this.currentSchoolYear}學年度 第${this.currentSemester}學期 ${this.curClass.class_name}` +
-    tableSelect.outerHTML
-    +` <br> 教師簽名: `);
-    
+    var tableHTML = encodeURIComponent(`  <meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'/><style> table, td {border:1px solid #dee2e6;  text-align: center;} table {border-collapse:collapse}</style>` +
+      `導師加減分-${this.currentSchoolYear}學年度 第${this.currentSemester}學期 ${this.curClass.class_name}` +
+      tableSelect.outerHTML
+      + ` <br> 教師簽名: `);
+
     // Specify file name
-    var filename ='導師加減分'+'.xls';
-    
+    var filename = '導師加減分' + '.xls';
+
     // Create download link element
     downloadLink = document.createElement("a");
-    
+
     document.body.appendChild(downloadLink);
-    
-    
-    if(navigator.msSaveOrOpenBlob){
-      
-        var blob = new Blob([tableHTML], {
-            type: dataType
-        });
-        navigator.msSaveOrOpenBlob( blob, filename);
+
+
+    if (navigator.msSaveOrOpenBlob) {
+
+      var blob = new Blob([tableHTML], {
+        type: dataType
+      });
+      navigator.msSaveOrOpenBlob(blob, filename);
     } else {
-        // Create a link to the file
-        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-    
-        // Setting the file name
-        downloadLink.download = filename;
-        
-        //triggering the function
-        downloadLink.click();
+      // Create a link to the file
+      downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+      // Setting the file name
+      downloadLink.download = filename;
+
+      //triggering the function
+      downloadLink.click();
     }
   }
 
@@ -271,6 +315,7 @@ interface ClassRec {
 }
 
 interface MoralScoreRec {
+  MoralScoreID: string;
   StudentID: string;
   Name: string;
   SbDiff: string;
