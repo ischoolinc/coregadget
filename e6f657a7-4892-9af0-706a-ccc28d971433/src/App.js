6c4e00237533733a709d63ddfc19d8db76814b79
CynthiaChen <cynthia.chen@ischool.com.tw>
2022-12-01 17:19:06 +0800

    加權平均/算術平均，改讀固定排名算出來的分數

--- e6f657a7-4892-9af0-706a-ccc28d971433/src/App.js
+++ e6f657a7-4892-9af0-706a-ccc28d971433/src/App.js
@@ -4,18 +4,15 @@ import { Routes, Route, Link } from 'react-router-dom';
 import RankDetail from './RankDetail';
 import Main from './Main';
 
-
-
 function App() {
 
   return (
     <div className="App">
-
+      {/* This is App.js Page. */}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/RankDetail" element={<RankDetail />} />
       </Routes>
-
     </div>
   );
 }
