12ef866291c10651927dcdb81be368d28ceecbbd
JoshKyo <josh.chiang@ischool.com.tw>
2024-01-10 12:53:02 +0800

    台大EMBA 學業成績 RWD 畫面優化

--- 6613201e-03d9-4b51-8f31-f5697be268dd/src/components/academic.jsx
+++ 6613201e-03d9-4b51-8f31-f5697be268dd/src/components/academic.jsx
@@ -1,135 +1,201 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import { contract } from '../utility';
 
 export default class Academic extends Component {
-    constructor(props) {
-        super(props);
-      
-        this.state = {
-            currentData: []
-        };
+  constructor(props) {
+    super(props);
 
-        this.getSemester = this.getSemester.bind(this);
-        this.getCurrentCourse = this.getCurrentCourse.bind(this);
-    }
-    
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-12" id="current-course">
-                    <table className="table table-bordered my-table">
-                        <thead>
-                            <tr>
-                                <th colSpan="11"><span><i className="glyphicon glyphicon-tag"></i> 本學期修課</span></th>
-                            </tr>
-                            <tr>
-                                <th>流水號</th>
-                                <th>學年度</th>
-                                <th>學期</th>
-                                <th>課程識別碼</th>
-                                <th>課程名稱</th>
-                                <th>班次</th>
-                                <th>類別</th>
-                                <th>必選修</th>
-                                <th>學分數</th>
-                                <th>授課教師</th>
-                                <th>成績</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {(this.state.currentData.length) ? this.state.currentData : <tr><td colSpan="11">目前無資料</td></tr>}
-                        </tbody>
-                    </table>
-                </div>
+    this.state = {
+      currentData: []
+    };
+
+    this.getSemester = this.getSemester.bind(this);
+    this.getCurrentCourse = this.getCurrentCourse.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="current-course" className='text-sm'>
+          <div className="card flex justify-between items-center heard-table cursor-pointer md:cursor-auto p-2"
+            style={{ background: '#dddde1', borderBottom: '1px solid #ACACAC' }}
+            data-toggle="collapse"
+            data-target="#classlist" aria-expanded="true" aria-controls="classlist">
+            <div><i className="glyphicon glyphicon-tag"></i> <span>本學期修課</span></div>
+            <div className="block md:hidden text-xl px-4">
+              <i className="collapse-close fa fa-angle-down font-bold text-xl" aria-hidden="true"></i>
+              <i className="collapse-open fa fa-angle-up font-bold text-xl" aria-hidden="true"></i>
             </div>
-        );
-    }
+          </div>
+          <div className="collapse in" id="classlist" aria-expanded="true">
+            <div className="dis-show mt-4">
+              {(this.state.currentData.length) ? this.state.currentData : <div>目前無資料</div>}
+            </div>
 
-    getSemester() {
-        return new Promise((resolve, reject) => {
-            contract.send({
-                service: "default.GetSemester",
-                body: {},
-                result: (response, error, http) => {
-                    if (!error) {
-                        if (response.Result) {
-                            let schoolYear = response.Result.SystemConfig.DefaultSchoolYear;
-                            let semester = response.Result.SystemConfig.DefaultSemester;
-                            resolve({schoolYear: schoolYear, semester: semester})
-                        } else {
-                            console.log('default.GetSemester 查無學年期');
-                            reject();
-                        }
-                    } else {
-                        console.log(error);
-                        reject();
-                    }
-                }
-            });
+          </div>
+          <table className="table my-table dis-none">
+            <thead>
+              <tr>
+                <th>流水號</th>
+                <th>學年度</th>
+                <th>學期</th>
+                <th>課程識別碼</th>
+                <th>課程名稱</th>
+                <th>班次</th>
+                <th>類別</th>
+                <th>必選修</th>
+                <th>學分數</th>
+                <th>授課教師</th>
+                <th>成績</th>
+              </tr>
+            </thead>
+            {(this.state.currentData.length) ? this.state.currentData : <tr><td colSpan="11">目前無資料</td></tr>}
+          </table>
+        </div>
+      </div>
+    );
+  }
+
+  getSemester() {
+    return new Promise((resolve, reject) => {
+      contract.send({
+        service: "default.GetSemester",
+        body: {},
+        result: (response, error, http) => {
+          if (!error) {
+            if (response.Result) {
+              let schoolYear = response.Result.SystemConfig.DefaultSchoolYear;
+              let semester = response.Result.SystemConfig.DefaultSemester;
+              resolve({ schoolYear: schoolYear, semester: semester })
+            } else {
+              console.log('default.GetSemester 查無學年期');
+              reject();
+            }
+          } else {
+            console.log(error);
+            reject();
+          }
+        }
+      });
+    });
+
+  };
+
+  // 取得當學期修課課程
+  getCurrentCourse(schoolYear, semester) {
+    return new Promise((resolve, reject) => {
+      contract.send({
+        service: "default.GetCurrentCourse",
+        body: {
+          Request: {
+            SchoolYear: schoolYear,
+            Semester: semester
+          }
+        },
+        result: (response, error, http) => {
+          if (!error) {
+            if (response.Result) {
+              resolve([].concat(response.Result.Course || []));
+            } else {
+              resolve([]);
+            }
+          } else {
+            console.log(error);
+            reject();
+          }
+        }
+      });
+    });
+  };
+
+  componentWillMount() {
+    let self = this;
+    this.getSemester().then((data) => {
+      // console.log(data);
+      this.getCurrentCourse(data.schoolYear, data.semester).then((lists) => {
+        // console.log(lists);
+        let items = [];
+        if (lists.length) {
+          lists.forEach((item, idx) => {
+            items.push(
+              <tbody className='contents'>
+                <tr key={idx} className="dis-none">
+                  <td>{item.SerialNo}</td>
+                  <td>{item.SchoolYear}</td>
+                  <td>{(item.Semester === "0" ? "夏季" : `第${item.Semester}學期`)}</td>
+                  <td>{item.NewSubjectCode}</td>
+                  <td>{item.SubjectName}</td>
+                  <td>{item.ClassName}</td>
+                  <td>{item.CourseType}</td>
+                  <td>{(item.IsRequired == "t") ? "必修" : "選修"}</td>
+                  <td>{item.Credit}</td>
+                  <td>{item.TeacherName}</td>
+                  <td>{item.IsCancel == 't' ? "停修" : item.Score}</td>
+                </tr>
+                <div className='dis-show'>
+                  <div className={`bd-callout px-4 pt-3 mb-5 ${(item.IsRequired == "t") ? 'requiredt' : 'requiredf'}`} >
+                    <div className="card text-muted cursor-pointer collapsed" aria-expanded="false" data-toggle="collapse" data-target="#classlist0" aria-controls="classlist0">
+                      <div className="flex justify-center items-center gap-x-2 pb-3">
+                        <div className={`text-nowrap ${(item.IsRequired == "t") ? 'requiredt' : 'requiredf'}`}>{(item.IsRequired == "t") ? "必修" : "選修"}</div>
+                        <div className="flex flex-col">
+                          <div className="text-base font-semibold color-blue">{item.SubjectName} {item.ClassName}</div>
+                          <div className="whitespace-pre-line">{item.SchoolYear}學年度 {(item.Semester === "0" ? "夏季" : `第${item.Semester}學期`)} ({item.IsCancel == 't' ? "停修" : item.Score})</div>
+                        </div>
+                        <div className="flex-grow text-xl text-right pr-2">
+                          <i className="collapse-close fa fa-angle-down font-bold text-xl" aria-hidden="true"></i>
+                          <i className="collapse-open fa fa-angle-up font-bold text-xl" aria-hidden="true"></i>
+
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-muted -mx-4 px-4 collapse" style={{ background: 'rgb(250, 250, 250)' }} id="classlist0" aria-expanded="false">
+                      <hr className="mb-2 mt-0" />
+                      <div className="flex justify-between mb-3">
+                        <div className="text-center">
+                          <div>流水號</div>
+                          <div className="font-bold">{item.SerialNo}</div>
+                        </div>
+                        <div className="text-center">
+                          <div>學分</div>
+                          <div className="font-bold">{item.Credit}</div>
+                        </div>
+                        <div className="text-center">
+                          <div>課程識別碼</div>
+                          <div className="font-bold">{item.NewSubjectCode}</div>
+                        </div>
+                        <div className="text-center">
+                          <div>班次</div>
+                          <div className="font-bold">{item.ClassName}</div>
+                        </div>
+                      </div>
+                      <div className="flex justify-start mb-3">
+                        <div className="mr-2">教師</div>
+                        <div>{item.TeacherName}</div>
+                      </div>
+                      <div className='grid grid-cols-2'>
+                        <div className="flex justify-start mb-3">
+                          <div className="mr-2">類別</div>
+                          <div className="font-bold">{item.CourseType}</div>
+                        </div>
+                        <div className="flex justify-start mb-3">
+                          <div className="mr-2">成績</div>
+                          <div className="font-bold">{item.IsCancel == 't' ? "停修" : item.Score}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </tbody>
+            );
+          });
+        }
+        self.setState((prevState, props) => {
+          return { currentData: items };
         });
-        
-    };
-
-    // 取得當學期修課課程
-    getCurrentCourse(schoolYear, semester) {
-        return new Promise((resolve, reject) => {
-            contract.send({
-                service: "default.GetCurrentCourse",
-                body: {
-                    Request: {
-                        SchoolYear: schoolYear,
-                        Semester: semester
-                    }
-                },
-                result: (response, error, http) => {
-                    if (!error) {
-                        if (response.Result) {
-                            resolve([].concat(response.Result.Course || []));
-                        } else {
-                            resolve([]);
-                        }
-                    } else {
-                        console.log(error);
-                        reject();
-                    }
-                }
-            });
-        });
-    };
-
-    componentWillMount() {
-        let self = this;
-        this.getSemester().then((data) => {
-            // console.log(data);
-            this.getCurrentCourse(data.schoolYear, data.semester).then((lists) => {
-                // console.log(lists);
-                let items = [];
-                if (lists.length) {
-                    lists.forEach((item, idx) => {
-                        items.push(
-                            <tr key={idx}>
-                                <td>{item.SerialNo}</td>
-                                <td>{item.SchoolYear}</td>
-                                <td>{(item.Semester === "0" ? "夏季" : `第${item.Semester}學期`)}</td>
-                                <td>{item.NewSubjectCode}</td>
-                                <td>{item.SubjectName}</td>
-                                <td>{item.ClassName}</td>
-                                <td>{item.CourseType}</td>
-                                <td>{(item.IsRequired=="t") ? "必修" : "選修"}</td>
-                                <td>{item.Credit}</td>
-                                <td>{item.TeacherName}</td>
-                                <td>{item.IsCancel == 't' ? "停修" : item.Score}</td>
-                            </tr>
-                        );
-                    });
-                }
-                self.setState((prevState, props) => {
-                    return { currentData: items };
-                });
-            })
-            .catch((err) => {});
-        })
-        .catch((err) => {});
-    };
+      })
+        .catch((err) => { });
+    })
+      .catch((err) => { });
+  };
 }
--- 6613201e-03d9-4b51-8f31-f5697be268dd/src/components/graduation_requirement.jsx
+++ 6613201e-03d9-4b51-8f31-f5697be268dd/src/components/graduation_requirement.jsx
@@ -1,160 +1,208 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import { contract } from '../utility';
 
 export default class GraduationRequirement extends Component {
-    constructor(props) {
-        super(props);
-      
-        this.state = {
-            info: '',
-            list: []
-        };
+  constructor(props) {
+    super(props);
 
-        this.getRaduationRequirement = this.getRaduationRequirement.bind(this);
-        this.getGraduationSubjectList = this.getGraduationSubjectList.bind(this);
-        this.getSubjectSemesterScore = this.getSubjectSemesterScore.bind(this);
+    this.state = {
+      info: '',
+      list: []
     };
 
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-12">
-                    <table className="table table-bordered my-table">
-                        <thead>
-                            <tr>
-                                <th colSpan="6">
-                                    <div>
-                                        <i className="glyphicon glyphicon-tag"></i>
-                                        <span id="credit-info-title">
-                                            {this.state.info}
-                                        </span>
-                                    </div>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th>課程識別碼</th>
-                                <th>課號</th>
-                                <th>課程名稱</th>
-                                <th>學分數</th>
-                                <th>群組別</th>
-                                <th>備註</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {(this.state.list.length) ? this.state.list : <tr><td colSpan="6">目前無資料</td></tr> }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
-    };
+    this.getRaduationRequirement = this.getRaduationRequirement.bind(this);
+    this.getGraduationSubjectList = this.getGraduationSubjectList.bind(this);
+    this.getSubjectSemesterScore = this.getSubjectSemesterScore.bind(this);
+  };
 
-    // 取得畢業學分表
-    getRaduationRequirement() {
-        contract.send({
-            service: "default.GetGraduationRequirement",
-            body: {},
-            result: (response, error, http) => {
-                if (!error) {
-                    if (response.Result) {
-                        this.setState((prevState, props) => {
-                            return { 
-                                info: `
-                                    畢業應修科目及學分表：系訂必修： ${response.Result.DepartmentCredit || ''} 
-                                    學分、選修： ${response.Result.ElectiveCredit || ''} 
-                                    學分、應修最低畢業學分： ${response.Result.RequiredCredit || ''}
-                                `
-                            };
-                        });
-                    }
-                }
-            }
-        });
-    };
-  
-    // 取得畢業應修科目
-    getGraduationSubjectList() {
-        return new Promise((resolve, reject) => {
-            contract.send({
-                service: "default.GetGraduationSubjectList",
-                body: {},
-                result: (response, error, http) => {
-                    if (!error) {
-                        if (response.Result) {
-                            resolve([].concat(response.Result.Subject || []));
-                        } else {
-                            resolve([]);
-                        }
-                    } else {
-                        console.log(error)
-                        reject();
-                    }
-                }
-            });
-        });
-    };
+  render() {
+    return (
+      <div className='text-sm mt-4'>
+        <div className="card flex justify-between items-center heard-table cursor-pointer md:cursor-auto p-2"
+          style={{ background: '#dddde1', borderBottom: '1px solid #ACACAC' }}
+          data-toggle="collapse"
+          data-target="#leaveSchool" aria-expanded="true" aria-controls="leaveSchool">
+          <div className='flex'><i className="glyphicon glyphicon-tag"></i> <pre className='p-0 pl-1'>{this.state.info}</pre></div>
+          <div className="block md:hidden text-xl pr-4">
+            <i className="collapse-close fa fa-angle-down font-bold text-xl" aria-hidden="true"></i>
+            <i className="collapse-open fa fa-angle-up font-bold text-xl" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div className="collapse in" id="leaveSchool" aria-expanded="true">
+          <div className="dis-show mt-4">
+            {(this.state.list.length) ? this.state.list : <tr><td colSpan="6">目前無資料</td></tr>}
+          </div>
 
-    // 取得所修課程成績
-    getSubjectSemesterScore() {
-        return new Promise((resolve, reject) => {
-            contract.send({
-                service: "default.GetSubjectSemesterScore",
-                body: {},
-                result: (response, error, http) => {
-                    if (!error) {
-                        if (response.Result) {
-                            let scoreTexts = {};
+        </div>
+        <table className="table my-table dis-none">
+          <thead>
+            <tr>
+              <th>課程識別碼</th>
+              <th>課號</th>
+              <th>課程名稱</th>
+              <th>學分數</th>
+              <th>群組別</th>
+              <th>備註</th>
+            </tr>
+          </thead>
+          <tbody>
+            {(this.state.list.length) ? this.state.list : <tr><td colSpan="6">目前無資料</td></tr>}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
-                            ([].concat(response.Result.Score || [])).forEach((item, idx) => {
-                                if (item.IsPass === 't' && (item.ScoreConfirmed === "" || item.ScoreConfirmed === "t")) {
-                                    scoreTexts['subjectId_' + item.SubjectID] = '已取得學分';
-                                } else {
-                                    scoreTexts['subjectId_' + item.SubjectID] = '';                                    
-                                }
-                            });
-                            resolve(scoreTexts);
-                        } else {
-                            resolve({});
-                        }
-                    } else {
-                        reject();
-                    }
-                }
-            });
-        });
-    };
-
-    componentWillMount() {
-        // 取得畢業學分表
-        this.getRaduationRequirement();
-
-        Promise.all([this.getGraduationSubjectList(), this.getSubjectSemesterScore()]).then((data) => {            
-            let items = [];
-            if (data[0].length) {
-                data[0].forEach((item, idx) => {
-                    items.push(
-                        <tr key={idx} className="info">
-                            <td>{item.NewSubjectCode}</td>
-                            <td>{item.SubjectCode}</td>
-                            <td>
-                                {item.ChineseName}<br />{item.EnglishName}
-                            </td>
-                            <td>{item.Credit}</td>
-                            <td>{item.GroupName}</td>
-                            <td>
-                                {data[1]['subjectId_' + item.SubjectID]}
-                            </td>
-                        </tr>
-                    )
-                });
-            }
+  // 取得畢業學分表
+  getRaduationRequirement() {
+    contract.send({
+      service: "default.GetGraduationRequirement",
+      body: {},
+      result: (response, error, http) => {
+        if (!error) {
+          if (response.Result) {
             this.setState((prevState, props) => {
-                return { list: items };
+              return {
+                info: 
+                `畢業應修科目及學分表：
+                系訂必修：${response.Result.DepartmentCredit || ''} 學分、選修：${response.Result.ElectiveCredit || ''} 學分、
+                應修最低畢業學分： ${response.Result.RequiredCredit || ''}
+                `
+              };
             });
+          }
+        }
+      }
+    });
+  };
 
-        },(err) => {
-            // console.log(err);
+  // 取得畢業應修科目
+  getGraduationSubjectList() {
+    return new Promise((resolve, reject) => {
+      contract.send({
+        service: "default.GetGraduationSubjectList",
+        body: {},
+        result: (response, error, http) => {
+          if (!error) {
+            if (response.Result) {
+              resolve([].concat(response.Result.Subject || []));
+            } else {
+              resolve([]);
+            }
+          } else {
+            console.log(error)
+            reject();
+          }
+        }
+      });
+    });
+  };
+
+  // 取得所修課程成績
+  getSubjectSemesterScore() {
+    return new Promise((resolve, reject) => {
+      contract.send({
+        service: "default.GetSubjectSemesterScore",
+        body: {},
+        result: (response, error, http) => {
+          if (!error) {
+            if (response.Result) {
+              let scoreTexts = {};
+
+              ([].concat(response.Result.Score || [])).forEach((item, idx) => {
+                if (item.IsPass === 't' && (item.ScoreConfirmed === "" || item.ScoreConfirmed === "t")) {
+                  scoreTexts['subjectId_' + item.SubjectID] = '已取得學分';
+                } else {
+                  scoreTexts['subjectId_' + item.SubjectID] = '';
+                }
+              });
+              resolve(scoreTexts);
+            } else {
+              resolve({});
+            }
+          } else {
+            reject();
+          }
+        }
+      });
+    });
+  };
+
+  componentWillMount() {
+    // 取得畢業學分表
+    this.getRaduationRequirement();
+
+    Promise.all([this.getGraduationSubjectList(), this.getSubjectSemesterScore()]).then((data) => {
+      let items = [];
+      if (data[0].length) {
+        data[0].forEach((item, idx) => {
+          items.push(
+            <tbody className='contents'>
+              <tr key={idx} className="info dis-none">
+                <td>{item.NewSubjectCode}</td>
+                <td>{item.SubjectCode}</td>
+                <td>
+                  {item.ChineseName}<br />{item.EnglishName}
+                </td>
+                <td>{item.Credit}</td>
+                <td>{item.GroupName}</td>
+                <td>
+                  {data[1]['subjectId_' + item.SubjectID]}
+                </td>
+              </tr>
+              <div className='dis-show'>
+                <div className="bd-callout requiredt info px-4 pt-3 mb-5" >
+                  <div className="card text-muted cursor-pointer collapsed" aria-expanded="false" data-toggle="collapse" data-target={`#leaveSchool${idx}`} aria-controls={`leaveSchool${idx}`}>
+                    <div className="flex justify-center items-center gap-x-2 pb-3">
+                      <div className="requiredt w-8" style={{flexShrink: '0'}}>{item.GroupName}</div>
+                      <div className="flex flex-col">
+                        <div className="text-base font-semibold color-blue">{item.ChineseName}</div>
+                        <div className="whitespace-pre-line">{item.EnglishName}</div>
+                      </div>
+                      <div className="flex-grow text-xl text-right pr-2">
+                        <i className="collapse-close fa fa-angle-down font-bold text-xl" aria-hidden="true"></i>
+                        <i className="collapse-open fa fa-angle-up font-bold text-xl" aria-hidden="true"></i>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-muted -mx-4 px-4 collapse" style={{ background: 'rgb(250, 250, 250)' }} id={`leaveSchool${idx}`} aria-expanded="false">
+                    <hr className="mb-2 mt-0" />
+                    <div className="flex justify-between mb-3">
+                      <div className="text-center">
+                        <div>課號</div>
+                        <div className="font-bold">{item.SubjectCode}</div>
+                      </div>
+                      <div className="text-center">
+                        <div>學分</div>
+                        <div className="font-bold">{item.Credit}</div>
+                      </div>
+                      <div className="text-center">
+                        <div>課程識別碼</div>
+                        <div className="font-bold">{item.NewSubjectCode}</div>
+                      </div>
+                    </div>
+                    <div className="flex justify-start mb-3">
+                      <div className="mr-2">群組別</div>
+                      <div className="font-bold">{item.GroupName}</div>
+                    </div>
+                    <hr className="my-2" />
+                    <div className="font-bold pb-2">{data[1]['subjectId_' + item.SubjectID]}</div>
+
+                  </div>
+                </div>
+              </div>
+            </tbody>
+          )
         });
-    }
+      }
+      this.setState((prevState, props) => {
+        return { list: items };
+      });
+
+    }, (err) => {
+      // console.log(err);
+    });
+  }
 }
